cc997fd74abb883d35ce66c6d637788c704b8194
axelofwar <tylercaton2350@gmail.com>
2023-03-24 17:09:40 -0400

    update FE for init wireframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import './App.css'
 import axios from 'axios';
 import React, { useState, useEffect } from "react";
-// import dotenv from 'dotenv';
-// dotenv.config();
-// const host = process.env.DATABASE_HOST
-// const database_api = process.env.LEADERBOARD_ENDPOINT
+
 
 function App() {
   const [data, setData] = useState([]);
   useEffect(() => {
     async function fetchData() {
-      try {
-        // const response = await axios.get("http://localhost:8000/api/leaderboard/");
-        const response = await axios.get("https://be-community-gamification.onrender.com/api/leaderboard/")
-        // const response = axios.get("https://"+host + database_api);
-        setData(response.data);
-      } catch (error) {
-        console.error(error);
-      }
+      fetch('https://be-community-gamification.onrender.com/api/leaderboard/')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          setData(data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
     }
 
     fetchData();
@@ -27,11 +25,12 @@ function App() {
 
   return (
     <div className="container">
-      <h1 className="title">Leaderboard</h1>
+      <img className="logo" src="/y00ts-logo.svg" alt="Y00TS Colors" />
+      <h2 className="title">Leaderboard</h2>
+      <img className="leaderboard" src="/y00ts-colors.png" alt="Yoots Logo" />
       <table className="table">
         <thead>
           <tr>
-            {/* <th>Index</th> */}
             <th>Name</th>
             <th>Favorites</th>
             <th>Retweets</th>
@@ -44,14 +43,13 @@ function App() {
         <tbody>
           {data && data.map((item) => (
             <tr key={item.index}>
-              {/* <td>{item.index}</td> */}
               <td>{item.Name}</td>
               <td>{item.Favorites}</td>
               <td>{item.Retweets}</td>
               <td>{item.Replies}</td>
               <td>{item.Impressions}</td>
               <td>{item.Rank}</td>
-              <td>{item.Global_Reach+"%"}</td>
+              <td>{item.Global_Reach + "%"}</td>
             </tr>
           ))}
         </tbody>
