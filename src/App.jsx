7c988662baf001873e129929c2958a975f5b40d6
axelofwar <tylercaton2350@gmail.com>
2023-03-30 21:00:40 -0400

    add description + url and format stats

--- src/App.jsx
+++ src/App.jsx
@@ -13,36 +13,60 @@ function CollapsibleTableRow(props) {
   return (
     <>
       <TableRow key={item.index} sx={{ '& > *': { borderBottom: 'unset' } }}>
-        <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>
-        <TableCell component="th" scope="row">
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <Avatar src={item.PFP_Url} alt={item.Name} sx={{ mr: 2 }} />
-            {item.Name}
+      <TableCell>
+        <IconButton
+          aria-label="expand row"
+          size="small"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+        </IconButton>
+      </TableCell>
+      <TableCell component="th" scope="row">
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Avatar src={item.PFP_Url} alt={item.Name} sx={{ mr: 2 }} />
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div>{item.Name}</div>
           </div>
-        </TableCell>
-        <TableCell align="right">{item.Rank}</TableCell>
-        <TableCell align="right">{item.Global_Reach + "%"}</TableCell>
-        <TableCell align="right">{item.Impressions}</TableCell>
-      </TableRow>
-      {open &&
-        <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={8}>
-            <div style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
-              <div>Favorites: {item.Favorites}</div>
-              <div>Retweets: {item.Retweets}</div>
-              <div>Replies: {item.Replies}</div>
+        </div>
+      </TableCell>
+      <TableCell align="right">{item.Rank}</TableCell>
+      <TableCell align="right">{item.Global_Reach + "%"}</TableCell>
+      <TableCell align="right">{item.Impressions}</TableCell>
+    </TableRow>
+    {open && (
+  <TableRow>
+    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={8}>
+      <div className='expanded-dropdown' style={{ display: "grid", gridTemplateColumns: "1fr 1fr", alignItems: "center" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div style={{ marginBottom: "1rem" }}>Bio: {item.Description}</div>
+          {item.Bio_Link !== "None" && (
+            <div>
+              Url:{" "}
+              <a href={item.Bio_Link} target="_blank" rel="noreferrer">
+                {item.Bio_Link}
+              </a>
             </div>
-          </TableCell>
-        </TableRow>
-      }
+          )}
+        </div>
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)" }}>
+          <div style={{ textAlign: "right" }}>Favorites:</div>
+          <div style={{ textAlign: "right" }}>Retweets:</div>
+          <div style={{ textAlign: "right" }}>Replies:</div>
+          <div style={{ textAlign: "right" }}>{item.Favorites}</div>
+          <div style={{ textAlign: "right" }}>{item.Retweets}</div>
+          <div style={{ textAlign: "right" }}>{item.Replies}</div>
+        </div>
+      </div>
+        {/* <div style={{ display: "grid", gridTemplateColumns: "1fr", alignItems: "right" }}>
+          <div style={{ marginBottom: "1rem", textAlign: "right" }}>Favorites: {item.Favorites}</div>
+          <div style={{ marginBottom: "1rem", textAlign: "right" }}>Retweets: {item.Retweets}</div>
+          <div style={{ marginBottom: "1rem", textAlign: "right" }}>Replies: {item.Replies}</div>
+        </div>
+      </div> */}
+    </TableCell>
+  </TableRow>
+)}
     </>
   );
 }
@@ -97,89 +121,3 @@ function App() {
 }
 
 export default App;
-
-// import './App.css'
-// // import axios from 'axios';
-// import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, IconButton } from '@mui/material';
-// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-// import Avatar from '@mui/material/Avatar';
-// import React, { useState, useEffect } from "react";
-// import '@fontsource/roboto/500.css';
-
-
-// function App() {
-//   const [data, setData] = useState([]);
-//   useEffect(() => {
-//     async function fetchData() {
-//       fetch('https://be-community-gamification.onrender.com/api/leaderboard/')
-//         .then(response => response.json())
-//         .then(data => {
-//           console.log(data);
-//           setData(data);
-//         })
-//         .catch(error => {
-//           console.error(error);
-//         });
-//     }
-
-//     fetchData();
-//   }, []);
-
-//   console.log(data);
-
-//   return (
-//     <div className="container">
-//       <img className="logo" src="/y00ts-logo.svg" alt="Y00TS Colors" />
-//       <h2 className="title">Leaderboard</h2>
-//       <img className="leaderboard" src="/y00ts-colors.png" alt="Yoots Logo" />
-//       <div style={{ marginTop: 20 }} />
-
-//       <TableContainer component={Paper}>
-//         <Table aria-label="collapsible table">
-//           <TableHead>
-//             <TableRow>
-//               <TableCell />
-//               <TableCell>Name</TableCell>
-//               <TableCell align="right">Favorites</TableCell>
-//               <TableCell align="right">Retweets</TableCell>
-//               <TableCell align="right">Replies</TableCell>
-//               <TableCell align="right">Impressions</TableCell>
-//               <TableCell align="right">Rank</TableCell>
-//               <TableCell align="right">Global Reach</TableCell>
-//             </TableRow>
-//           </TableHead>
-//           <TableBody>
-//             {data && data.map((item) => (
-//               <TableRow key={item.index} sx={{ '& > *': { borderBottom: 'unset' } }}>
-//                 <TableCell>
-//                   <IconButton
-//                     aria-label="expand row"
-//                     size="small"
-//                     onClick={() => console.log("clicked")}
-//                   >
-//                     <KeyboardArrowDownIcon />
-//                   </IconButton>
-//                 </TableCell>
-//                 <TableCell component="th" scope="row">
-//                 <div style={{ display: "flex", alignItems: "center" }}>
-//                   <Avatar src={item.PFP_Url} alt={item.Name} sx={{ mr: 2 }} />
-//                   {item.Name}
-//                 </div>
-//                 </TableCell>
-//                 <TableCell align="right">{item.Favorites}</TableCell>
-//                 <TableCell align="right">{item.Retweets}</TableCell>
-//                 <TableCell align="right">{item.Replies}</TableCell>
-//                 <TableCell align="right">{item.Impressions}</TableCell>
-//                 <TableCell align="right">{item.Rank}</TableCell>
-//                 <TableCell align="right">{item.Global_Reach + "%"}</TableCell>
-//               </TableRow>
-//             ))}
-//           </TableBody>
-//         </Table>
-//       </TableContainer>
-//     </div>
-//   );
-// }
-
-// export default App;
-
